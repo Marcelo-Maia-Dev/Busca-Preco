607e91613b2eba4788b8f0f040946b5d99c20d62
Marcelo Maia <marcelo.maia962@gmail.com>
2023-05-28 21:28:14 -0300

    html

--- projeto_preco/app_buscapreco/views.py
+++ projeto_preco/app_buscapreco/views.py
@@ -1,7 +1,14 @@
 from django.shortcuts import render
+from .models import Produto 
+
 
 def pesquisar(request):
     return render(request, 'produtos/pesquisa.html')
 
 def exibir_resultados(request):
-    pass
+    nome_produto = request.POST.get('produto')
+    print(nome_produto)
+    dados = {
+        "dados": Produto.objects.filter(nome__icontains=nome_produto).order_by('preco')  
+    }
+    return render(request,'protudos/resultado_pesquisa.html',dados)
