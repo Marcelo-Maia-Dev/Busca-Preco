266362749b614570ec5809170c25bc8f65a34c86
Marcelo_Maia <33229102+Marcelo-Maia-Dev@users.noreply.github.com>
2023-06-04 20:07:24 -0300

    Add files via upload

--- app.py
+++ app.py
@@ -9,4 +9,26 @@ from selenium.webdriver.suport.ui import WebDriverWait
 from selenium.webdriver.chrome.service import Service as ChromeService
 from selenium.webdriver.suport import expected_conditions as condicao_esperada
 
+conexao = psycopg2.connect(
+    database='railway',
+    user='postgres',
+    password='iLmWOVPGZU4fdqNqJxRl',
+    host='containers-us-west-142.railway.app',
+    port='5465'
+)
 
+sql = conexao.cursor()
+
+def novo_produto(sql,conexao,nome,preco,site,data_cotacao,link_imagem):
+    #Verificar se existe um produto igual já cadastrado
+    query = "SELECT * FROM app_buscapreco_produto WHERE nome=%s and preco=%s and site=%s"
+    valores = (nome, preco, site)
+    resutado = sql.execute(query, valores)
+    dados = sql.fetchall()
+
+    if len(dados) == 0: 
+        #Se não houver dados iguais, gravar um novo produto
+        query = "INSERT INTO app_buscapreco_produto(nome,preco,site,data_cotacao,link_imagem) VALUES(%S,%S,%S,%S,%S)"
+        valores = (nome,preco,site,data_cotacao,link_imagem)
+    #Se já houver dados iguais, não gravar um novo produto
+    else: 
