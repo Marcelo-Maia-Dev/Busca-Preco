74a7237d5e481c4db499b433e7526230d01f4836
Marcelo_Maia <33229102+Marcelo-Maia-Dev@users.noreply.github.com>
2023-06-08 17:06:36 -0300

    Add files via upload

--- app.py
+++ app.py
@@ -38,3 +38,36 @@ def novo_produto(sql,conexao,nome,preco,site,data_cotacao,link_imagem):
 conexao.commit()
 
 # Criar um web scraper(atras do selenium)
+
+# Iniciar o webdriver
+
+
+def iniciar_driver():
+    chrome_options = Options()
+
+    arguments = ['--lang=en-US', '--window-size=1300,1000',
+                 '--incognito']
+
+    for argument in arguments:
+        chrome_options.add_argument(argument)
+
+    chrome_options.add_experimental_option('prefs', {
+        'download.prompt_for_download': False,
+        'profile.default_content_setting_values.notifications': 2,
+        'profile.default_content_setting_values.automatic_downloads': 1
+
+    })
+
+    driver = webdriver.Chrome(service=ChromeService(
+        ChromeDriverManager().install()), options=chrome_options)
+    wait = WebDriverWait(
+        driver,
+        10,
+        poll_frequency=1,
+        ignored_exceptions=[
+            NoSuchElementException,
+            ElementNotVisibleException,
+            ElementNotSelectableException,
+        ]
+    )
+    return driver, wait
